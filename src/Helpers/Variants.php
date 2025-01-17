30b96abe122d720e3ba7b26ec0ac0c1442ce5fac
Olaiya Segun <vadeshayo@gmail.com>
2025-01-17 11:59:13 +0100

    CAPS-352: Added Variants

--- src/Helpers/Variants.php
+++ src/Helpers/Variants.php
@@ -2,7 +2,10 @@
 
 namespace Dan\Shopify\Helpers;
 
+use Dan\Shopify\ArrayGraphQL;
 use Dan\Shopify\Exceptions\GraphQLEnabledWithMissingQueriesException;
+use Dan\Shopify\Util;
+use Illuminate\Support\Arr;
 
 /**
  * Class Variants.
@@ -14,10 +17,117 @@ class Variants extends Endpoint
         return parent::useGraphQL('variants');
     }
 
-    public function ensureGraphQLSupport(): void
+    public function makeGraphQLQuery(): array
     {
-        if ($this->graphQLEnabled()) {
-            throw new GraphQLEnabledWithMissingQueriesException();
+        return $this->dto->mutate ? $this->getMutation() : $this->getQuery();
+    }
+
+    public function getFields()
+    {
+        return [
+            'barcode',
+            'compareAtPrice',
+            'createdAt',
+            'selectedOptions' => [
+                'name',
+                'value',
+            ],
+            'inventoryItem' => [
+                'id',
+                'requiresShipping',
+                'measurement' => [
+                    'id',
+                    'weight' => [
+                        'unit',
+                        'value',
+                    ],
+                ],
+                'inventoryLevels($PER_PAGE)' => [
+                    'edges' => [
+                        'node' => [
+                            'location' => [
+                                'fulfillmentService' => [
+                                    'id',
+                                    'handle',
+                                    'serviceName',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'id',
+            'image' => [
+                'id',
+            ],
+            'inventoryPolicy',
+            'inventoryQuantity',
+            'position',
+            'price',
+            'sku',
+            'taxable',
+            'title',
+            'updatedAt',
+        ];
+    }
+
+    private function getQuery()
+    {
+        if ($this->dto->getResourceId()) {
+            return $this->getVariant();
         }
+
+        throw new GraphQLEnabledWithMissingQueriesException('You cannot get variants directly. Use the Products endpoint');
+    }
+
+    private function getVariant()
+    {
+        $fields = [
+            'node($INPUT)' => [
+                '... on ProductVariant' => $this->getFields(),
+            ],
+        ];
+
+        return [
+            'query' => ArrayGraphQL::convert($fields, [
+                '$INPUT' => Util::toGraphQLIdParam($this->dto->getResourceId(), 'ProductVariant'),
+                '$PER_PAGE' => 'first: 250',
+            ]),
+            'variables' => null,
+        ];
+    }
+
+    private function getMutation(): array
+    {
+        if ($this->dto->getResourceId()) {
+            return $this->updateMutation();
+        }
+
+        throw new GraphQLEnabledWithMissingQueriesException('Mutation not supported yet');
+    }
+
+    private function updateMutation(): array
+    {
+        $query = [
+            'variantUpdate($INPUT)' => [
+                'variant' => $this->getFields(),
+                'userErrors' => [
+                    'field',
+                    'message',
+                ],
+            ],
+        ];
+
+        $variables = Util::convertKeysToCamelCase(Arr::get($this->dto->payload, 'variant'));
+        $variables['id'] = $this->dto->getResourceId('Variant');
+
+        return [
+            'query' => ArrayGraphQL::convert(
+                $query,
+                ['$INPUT' => 'input: $input', '$PER_PAGE' => 'first: 250'],
+                'mutation UpdateVariant($input: ProductVariantInput!)'
+            ),
+            'variables' => ['input' => $variables],
+        ];
     }
 }
