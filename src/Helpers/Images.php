c9c2d627ae34ac4e70105f3fd3bcba8ec3807c39
Åsbjørn Hansen <aasbjorn.hansen@gmail.com>
2025-01-21 14:36:27 +0100

    Add mutations and wip on images GET requests

--- src/Helpers/Images.php
+++ src/Helpers/Images.php
@@ -3,6 +3,7 @@
 namespace Dan\Shopify\Helpers;
 
 use Dan\Shopify\ArrayGraphQL;
+use Dan\Shopify\Exceptions\GraphQLEnabledWithMissingQueriesException;
 use Dan\Shopify\Exceptions\InvalidGraphQLCallException;
 use Dan\Shopify\Util;
 use Exception;
@@ -32,7 +33,7 @@ class Images extends Endpoint
      */
     protected function getQuery(): array
     {
-        if ($this->dto->hasResourceInQueue('products')) {
+        if ($this->dto->hasResourceInQueue('products') && ! $this->dto->getResourceId()) {
             return $this->constructImagesQuery();
         }
 
@@ -43,8 +44,20 @@ class Images extends Endpoint
         throw new InvalidGraphQLCallException('Invalid request, you must specify a resource (product, metafields etc.) or an image id.');
     }
 
-    protected function getMutation()
+    /**
+     * @throws GraphQLEnabledWithMissingQueriesException
+     */
+    private function getMutation(): array
     {
+        if ($this->dto->hasResourceInQueue('delete')) {
+            return $this->deleteMutation();
+        }
+
+        if ($this->dto->getResourceId()) {
+            return $this->updateMutation();
+        }
+
+        throw new GraphQLEnabledWithMissingQueriesException('Creating a image is currently not implemented');
     }
 
     /**
@@ -71,6 +84,31 @@ class Images extends Endpoint
     {
         return [
             'id',
+            'media($COUNT)' => [
+                'nodes' => [
+                    'id',
+                    '... on MediaImage' => [
+                        'id',
+                        'createdAt',
+                        'updatedAt',
+                        'image' => [
+                            'url',
+                            'altText',
+                            'width',
+                            'height',
+                        ],
+                    ],
+                ],
+            ],
+            'images($COUNT)' => [
+                'nodes' => [
+                    'id',
+                    'url',
+                    'altText',
+                    'width',
+                    'height',
+                ],
+            ],
             'variants($COUNT)' => [
                 'edges' => [
                     'node' => [
@@ -108,10 +146,10 @@ class Images extends Endpoint
     {
         return [
             '... on MediaImage' => [
+                'id',
                 'createdAt',
                 'updatedAt',
                 'image' => [
-                    'id',
                     'url',
                     'altText',
                     'width',
@@ -119,5 +157,80 @@ class Images extends Endpoint
                 ],
             ],
         ];
+    }
+
+    private function deleteMutation(): array
+    {
+        $fields = [
+            'productDeleteMedia' => [
+                'productDeleteMedia(mediaIds: [$mediaId], productId: $productId)' => [
+                    'deletedMediaIds',
+                    'deletedProductImageIds',
+                    'mediaUserErrors' => [
+                        'field',
+                        'message',
+                    ],
+                    'product' => [
+                        'id',
+                        'title',
+                        'media' => [
+                            'nodes' => [
+                                'alt',
+                                'mediaContentType',
+                                'status',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        return [
+            'query' => ArrayGraphQL::convert(
+                $fields,
+                [
+                    '$mediaId' => 'mediaId: $mediaId',
+                    '$productId' => 'productId: $productId',
+                ],
+                'mutation DeleteMediaImage'
+            ),
+            'variables' => [
+                'mediaId' => Util::toGid($this->dto->getResourceId(), 'MediaImage'),
+                'productId' => Util::toGid($this->dto->findResourceIdInQueue('products'), 'Product'),
+            ],
+        ];
+    }
+
+    private function updateMutation()
+    {
+        $fields = [
+            'fileUpdate' => [
+                'fileUpdate(files: $input)' => [
+                    'files' => [
+                        '... on MediaImage' => [
+                            'id',
+                            'image' => [
+                                'url',
+                            ],
+                        ],
+                    ],
+                    'userErrors' => [
+                        'message',
+                    ],
+                ],
+            ],
+        ];
+
+        $query = ArrayGraphQL::convert(
+            $fields,
+            [
+                '$input' => [
+                    'id' => Util::toGid($this->dto->getResourceId(), 'MediaImage'),
+                ],
+            ],
+            'mutation fileUpdate',
+        );
+
+        return $query;
     }
 }
